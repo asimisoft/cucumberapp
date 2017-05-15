71b8c3377d65b5308df4c9029844d6e015bcb6fd
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-05-15 09:24:23 +0545

    rspec tested

---
+++ app/models/test.rb
@@ -0,0 +1,4 @@
+class Test < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+end
---
+++ spec/factories/tests.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :test do
+    name "shiv raj"
+  end
+end
